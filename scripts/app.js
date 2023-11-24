0d353624b733f60af9835146196e2e0d68274b9f
Ben Selby <benmatselby@gmail.com>
2023-11-24 21:17:08 +0000

    We have all been to the UAE

--- scripts/app.js
+++ scripts/app.js
@@ -1,121 +1,110 @@
-$(function() {
+$(function () {
+  var PlacesView = Backbone.View.extend({
+    el: $("#world-app"),
 
-    var PlacesView = Backbone.View.extend({
+    showHim: true,
+    showHer: true,
+    showUs: true,
+    showAll: true,
 
-        el: $("#world-app"),
+    me: ["GB", "FR", "IN", "DE", "AE", "US", "BE", "IT", "SI", "ES", "GR", "EG", "CY"],
+    her: ["GB", "FR", "IE", "US", "BE", "IT", "SI", "ES", "GR", "EG", "AE"],
+    us: ["GB", "FR", "US", "BE", "IT", "SI", "ES", "GR", "EG"],
+    all: ["GB", "FR", "ES", "AE"],
 
-        showHim: true,
-        showHer: true,
-        showUs: true,
-        showAll: true,
+    R: Raphael("the-map", 1000, 700),
 
-        me: ['GB', 'FR', 'IN', 'DE', 'AE', 'US', 'BE', 'IT', 'SI', 'ES', 'GR', 'EG', 'CY'],
-        her: ['GB', 'FR', 'IE', 'US', 'BE', 'IT', 'SI', 'ES', 'GR', 'EG'],
-        us: ['GB', 'FR', 'US', 'BE', 'IT', 'SI', 'ES', 'GR', 'EG'],
-        all: ['GB', 'FR', 'ES'],
+    meLook: {
+      fill: "#337ab7",
+      stroke: "#fff",
+      "stroke-opacity": "1",
+      "stroke-linejoin": "round",
+      "stroke-width": "0.75",
+      "stroke-dasharray": "none",
+    },
 
-        R: Raphael("the-map", 1000, 700),
+    herLook: {
+      fill: "#a94442",
+      stroke: "#fff",
+      "stroke-opacity": "1",
+      "stroke-linejoin": "round",
+      "stroke-miterlimit": "4",
+      "stroke-width": "0.75",
+      "stroke-dasharray": "none",
+    },
 
-        meLook: {
-            "fill": "#337ab7",
-            "stroke": "#fff",
-            "stroke-opacity": "1",
-            "stroke-linejoin": "round",
-            "stroke-width": "0.75",
-            "stroke-dasharray": "none"
-        },
+    usLook: {
+      fill: "#6f5499",
+      stroke: "#fff",
+      "stroke-opacity": "1",
+      "stroke-linejoin": "round",
+      "stroke-miterlimit": "4",
+      "stroke-width": "0.75",
+      "stroke-dasharray": "none",
+    },
 
-        herLook: {
-            "fill": "#a94442",
-            "stroke": "#fff",
-            "stroke-opacity": "1",
-            "stroke-linejoin": "round",
-            "stroke-miterlimit": "4",
-            "stroke-width": "0.75",
-            "stroke-dasharray": "none"
-        },
+    allLook: {
+      fill: "#5cb85c",
+      stroke: "#fff",
+      "stroke-opacity": "1",
+      "stroke-linejoin": "round",
+      "stroke-miterlimit": "4",
+      "stroke-width": "0.75",
+      "stroke-dasharray": "none",
+    },
 
-        usLook: {
-            "fill": "#6f5499",
-            "stroke": "#fff",
-            "stroke-opacity": "1",
-            "stroke-linejoin": "round",
-            "stroke-miterlimit": "4",
-            "stroke-width": "0.75",
-            "stroke-dasharray": "none"
-        },
+    none: {
+      fill: "grey",
+      stroke: "#fff",
+      "stroke-opacity": "1",
+      "stroke-linejoin": "round",
+      "stroke-miterlimit": "4",
+      "stroke-width": "0.75",
+      "stroke-dasharray": "none",
+    },
 
-        allLook: {
-            "fill": "#5cb85c",
-            "stroke": "#fff",
-            "stroke-opacity": "1",
-            "stroke-linejoin": "round",
-            "stroke-miterlimit": "4",
-            "stroke-width": "0.75",
-            "stroke-dasharray": "none"
-        },
+    events: {
+      "click .legend": "toggle",
+    },
 
-        none: {
-            "fill": "grey",
-            "stroke": "#fff",
-            "stroke-opacity": "1",
-            "stroke-linejoin": "round",
-            "stroke-miterlimit": "4",
-            "stroke-width": "0.75",
-            "stroke-dasharray": "none"
-        },
+    initialize: function () {
+      this.render();
+    },
 
-        events: {
-            "click .legend": "toggle"
-        },
-
-        initialize: function() {
-            this.render();
-        },
-
-        render: function() {
-            for (var country in world) {
-                if (this.all.indexOf(country) >= 0 && this.showAll === true) {
-
-                    this.R.path(world[country]).attr(this.allLook);
-
-                } else if (this.us.indexOf(country) >= 0 && this.showUs === true) {
-
-                    this.R.path(world[country]).attr(this.usLook);
-
-                } else if (this.her.indexOf(country) >= 0 && this.showHer === true) {
-
-                    this.R.path(world[country]).attr(this.herLook);
-
-                } else if (this.me.indexOf(country) >= 0 && this.showHim === true) {
-
-                    this.R.path(world[country]).attr(this.meLook);
-
-                } else {
-                    this.R.path(world[country]).attr(this.none);
-                }
-            }
-        },
-
-        toggle: function(event) {
-
-            let persona = $(event.currentTarget).data('who');
-
-            if (['Him', 'Her', 'Us', 'All'].indexOf(persona) === -1) {
-                return;
-            }
-
-            if (this['show'+persona] === true) {
-                this['show'+persona] = false;
-            } else {
-                this['show'+persona] = true;
-            }
-
-            $(event.currentTarget).toggleClass('on');
-
-            this.render();
+    render: function () {
+      for (var country in world) {
+        if (this.all.indexOf(country) >= 0 && this.showAll === true) {
+          this.R.path(world[country]).attr(this.allLook);
+        } else if (this.us.indexOf(country) >= 0 && this.showUs === true) {
+          this.R.path(world[country]).attr(this.usLook);
+        } else if (this.her.indexOf(country) >= 0 && this.showHer === true) {
+          this.R.path(world[country]).attr(this.herLook);
+        } else if (this.me.indexOf(country) >= 0 && this.showHim === true) {
+          this.R.path(world[country]).attr(this.meLook);
+        } else {
+          this.R.path(world[country]).attr(this.none);
         }
-    });
+      }
+    },
 
-    var App = new PlacesView;
+    toggle: function (event) {
+      let persona = $(event.currentTarget).data("who");
+
+      if (["Him", "Her", "Us", "All"].indexOf(persona) === -1) {
+        return;
+      }
+
+      if (this["show" + persona] === true) {
+        this["show" + persona] = false;
+      } else {
+        this["show" + persona] = true;
+      }
+
+      $(event.currentTarget).toggleClass("on");
+
+      this.render();
+    },
+  });
+
+  var App = new PlacesView();
 });
